0ce06d38d4c7e45969cffaabe68ae45d398b5cbd
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-30 11:23:24 -0300

    nothing important has change

--- src/features/containers/ShoppingCart-container.tsx
+++ src/features/containers/ShoppingCart-container.tsx
@@ -1,17 +1,15 @@
 import React, { useState } from 'react';
-import Img from '../../components/img/Img';
 import { Container } from '../../_assets/container-styled';
-import ButtonComponents from '../../components/button/Button';
 export default function ShoppingCartContainer() {
 
   return (
 
     <>
       <Container>
-        <Img/>
+        Hello world!
 
       </Container>
-      <ButtonComponents/>
+
     </>
 
 
