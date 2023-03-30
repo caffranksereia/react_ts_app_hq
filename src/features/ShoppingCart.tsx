d4917f5d73808b75991fe98f33a4dfda6a25a45a
fabio eduardo <circuncisaofabio@icloud.com>
2023-03-30 11:17:18 -0300

    add Heade

--- src/features/ShoppingCart.tsx
+++ src/features/ShoppingCart.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Container } from '../_assets/container-styled';
 import ShoppingCartContainer from './containers/ShoppingCart-container';
+import Header from '../components/header/Header';
 
 export default function ShoppingCart() {
   return (
     <Container>
+      <Header/>
       <ShoppingCartContainer/>
     </Container>
   );
